8f7ea4603897426ac9747a5a2b2a4205a1f89f8a
Dember <salinas.dember@gmail.com>
2025-11-25 22:45:26 -0600

    Emparejamiento maximo added

---
+++ 8 - Emparejamiento maximo/Edmonds.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Graph.h"
+
+void startBFS(Graph& g);
+
+/*
+En esta funcion podemos encontrar la distancia entre dos nodos.
+
+La complejidad de BFS es de O(V+E).
+*/
---
+++ 8 - Emparejamiento maximo/Karp.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Graph.h"
+
+void startKarp(Graph& g);
+
+/*
+En esta funcion podemos encontrar la cantidad de caminos, el camino mas corto y el camino mas 
+largo entre dos nodos.
+
+Backtracking suele tener una complejidad de O(n!), pero en este caso tiene comlejidad O(2^V), ya
+que no estamos buscando permutaciones, sino que únicamente combinaciones, al no permitir caminos
+con ciclos.
+*/
