10af116f5df2df3e14e8b99ac119cdf77a0b32a2
Dember <salinas.dember@gmail.com>
2025-11-28 19:05:24 -0600

    Kosaraju ended

--- 2 - Componentes conexos/Kosaraju.cpp
+++ 2 - Componentes conexos/Kosaraju.cpp
@@ -3,50 +3,51 @@
 
 using namespace std;
 
-void dfsOrden(int u, const Graph& g, vector<int>& vis, vector<int>& orden) {
-    vis[u] = 1;
-    for (const Edge& e : g.adj[u]) {
-        int v = e.to;
-        if (!vis[v]) dfsOrden(v, g, vis, orden);
+void dfsOrden(int u, Graph& g, vector<int>& vis, vector<int>& orden){
+    vis[u]=1;
+    for(auto& e:g.adj[u]){
+        int v=e.to;
+        if(!vis[v])dfsOrden(v, g, vis, orden);
     }
     orden.push_back(u);
+
+    return;
 }
 
-void dfsComponente(int u,
-    const vector<vector<int>>& radj,vector<int>& vis,vector<int>& compActual) {
-    vis[u] = 1;
+void dfsComponente(int u, vector<vector<int>>& radj,vector<int>& vis,vector<int>& compActual){
+    vis[u]=1;
     compActual.push_back(u);
-    for (int v : radj[u]) {
-        if (!vis[v]) dfsComponente(v, radj, vis, compActual);
+    for(int v:radj[u]){
+        if(!vis[v])dfsComponente(v, radj, vis, compActual);
     }
+
+    return;
 }
 
-void startKosaraju(const Graph& g) {
-    int n = g.n;
-
-    vector<int> vis(n, 0);
+void startKosaraju(Graph& g){
+    vector<int> vis(g.n, 0);
     vector<int> orden;
-    orden.reserve(n);
+    orden.reserve(g.n);
 
-    for (int i = 0; i < n; ++i) {
-        if (!vis[i]) dfsOrden(i, g, vis, orden);
+    for(int i=0; i<g.n; i++){
+        if(!vis[i])dfsOrden(i, g, vis, orden);
     }
 
-    vector<vector<int>> radj(n);
-    for (int u = 0; u < n; ++u) {
-        for (const Edge& e : g.adj[u]) {
-            int v = e.to;
+    vector<vector<int>> radj(g.n);
+    for(int u=0; u<g.n; u++){
+        for(auto& e:g.adj[u]){
+            int v=e.to;
             radj[v].push_back(u);
         }
     }
 
     fill(vis.begin(), vis.end(), 0);
     vector<vector<int>> componentes;
-    int numSCC = 0;
+    int numSCC=0;
 
-    for (int i = (int)orden.size() - 1; i >= 0; --i) {
-        int u = orden[i];
-        if (!vis[u]) {
+    for(int i=orden.size()-1; i>=0; i--){
+        int u=orden[i];
+        if(!vis[u]){
             vector<int> compActual;
             dfsComponente(u, radj, vis, compActual);
             componentes.push_back(compActual);
@@ -54,14 +55,15 @@ void startKosaraju(const Graph& g) {
         }
     }
 
-    cout << "\nComponentes fuertemente conexas (Kosaraju):\n";
-    cout << "Total de SCC: " << numSCC << "\n\n";
+    cout<<"\nComponentes fuertemente conexas (Kosaraju):\n";
+    cout<<"Total de SCC: "<<numSCC<<"\n\n";
 
-    int id = 1;
-    for (auto& comp : componentes) {
-        cout << "SCC " << id++ << ": ";
-        for (int v : comp) cout << v << " ";
-        cout << "\n";
-    }
-    cout << "\n";
+    int id=1;
+    for(auto& comp:componentes){
+        cout<<"SCC "<<id++<<": ";
+        for(auto& v:comp)cout<<v<<" ";
+        cout<<"\n";
+    }cout<<"\n";
+
+    return;
 }
