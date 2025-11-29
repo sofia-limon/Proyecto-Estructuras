67178c43ee90f32bc4043e9d7a3494ae29bf45f5
Dember <salinas.dember@gmail.com>
2025-11-28 18:22:51 -0600

    DSU added

--- 2 - Componentes conexos/DSU.h
+++ 2 - Componentes conexos/DSU.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "Graph.h"
 
-void startDSU(const Graph& g);
+void startDSU(Graph& g);
 
 /*
 Nos ayuda a encontrar La cantidad de componentes tanto en un grafo dirigio como en uno no dirigido.
